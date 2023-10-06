c4949d466a67570f10ca1ea9026ef8b6d62224a1
Lucas0356 <lucasmoltedo03@gmail.com>
2023-10-06 17:11:55 -0300

    06 cambio en etiqueta Image

--- 06-nextjs-app/app/posts/error.jsx
+++ 06-nextjs-app/app/posts/error.jsx
@@ -4,7 +4,7 @@ export default function Error() {
     return (
         <article>
             <p>No se han encontrados posts</p>
-            <Image
+            <img
                 src='https://http.cat/images/404.jpg'
                 alt='error 404 not found image cat'
                 width={300}
